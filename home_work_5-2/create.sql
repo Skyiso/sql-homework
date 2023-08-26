9292e43c0453a6a9e4483763319ec545629f33c0
Irina Zhelovatykh <skyy.isoo@gmail.com>
2023-08-26 15:12:33 +0500

    Added create.sql

---
+++ home_work_5-2/create.sql
@@ -0,0 +1,46 @@
+create table if not exists collections (
+	collectId SERIAL primary key,
+	collection_name varchar(30) not null,
+	collection_year integer not null
+);
+
+create table if not exists tracks (
+	trId SERIAL primary key,
+	track_name varchar(30) not null,
+	track_duration time(0) not null,
+	albId integer not null references albums(albId)
+);
+
+create table if not exists collectionsTracks (
+	collectId integer references collections(collectId),
+	trId integer references tracks(trId),
+	constraint pk primary key(collectId, trId)
+);
+
+create table if not exists albums (
+	albId SERIAL primary key,
+	album_name varchar(30) not null,
+	album_year integer not null
+);
+
+create table if not exists singers (
+	singId SERIAL primary key,
+	singer_name varchar(40) not null
+);
+
+create table if not exists albumsSingers (
+	singId integer references singers(singId),
+	albId integer references albums(albId),
+	constraint pk primary key(albId, singId)
+);
+
+create table if not exists genres (
+	genId SERIAL primary key,
+	genre_name varchar(20) not null
+);
+
+create table if not exists singersGenre (
+	genId integer references genres(genId),
+	singId integer references singers(singId),
+	constraint pk primary key(genId, singId)
+);
